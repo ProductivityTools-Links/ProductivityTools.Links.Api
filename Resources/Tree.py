27c0bfe0e3010c6564b32953c1863db29ba8a2b5
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-08-05 06:40:34 +0200

    tree model

--- Resources/Tree.py
+++ Resources/Tree.py
@@ -1,6 +1,19 @@
 from flask_restful import Resource
 from flask import Response
+from Models.Tree import Tree
+import os
 
 class TreeResource(Resource):
     def get(self):
+        uri = "neo4j+s://8345876f.databases.neo4j.io"
+        user = "neo4j"
+        password = os.getenv('password')
+        #password = "Kp9gl8g7YWx9XDrqAW"
+        app = Tree(uri, user, password)
+        app.create_friendship("Alice", "David")
+        app.find_person("Alice")
+        app.close()
+
+
+
         return Response("fdsa");
