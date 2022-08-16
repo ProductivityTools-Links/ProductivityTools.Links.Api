ed3a044006dfb0b5c7978ac444f892c52423fe8a
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-08-16 21:22:47 +0200

    next steps

--- Models/Tree.py
+++ Models/Tree.py
@@ -14,7 +14,15 @@ class Tree():
         query=(
             'MATCH(a:account{login:$login}) - [k:CHILD *]->(n:Node) return id(n) as idn, n, id(a) as ida, a, k'
         )
+        #read the account information
+        #read the nodes what is done and start creating a tree
         result=tx.run(query,login=login)
+        for element in result:
+            print(element[0])
+            print(element[1]._properties['name'])
+            print('is a child of')
+            print(element[2])
+            print(element)
         return [{"id":row["k"][0].id,type:row["k"][0].type, "nodes":row["k"][0].nodes } for row in result]
         #return [{"node":{"id": row["idn"] ,"name":row["n"]["name"]}, "account":{ "id": row["ida"] ,"name":row["a"]["login"]},"relation":{"r":row["k"]}} for row in result]
 
