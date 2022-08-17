6a4aa16761f240a4cc7a986f8d0764044be51ad9
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-08-17 21:49:00 +0200

    get account from db

---
+++ DTO/Account.py
@@ -0,0 +1,5 @@
+class Account:
+    def __init__(self,id,login):
+        self.id=id;
+        self.login=login
+        self.nodes=[]
--- Models/Tree.py
+++ Models/Tree.py
@@ -1,4 +1,6 @@
 from neo4j import  GraphDatabase
+from DTO.Account import Account
+
 
 class Tree():
     def __init__(self, uri, user, password):
@@ -11,6 +13,7 @@ class Tree():
 
     @staticmethod
     def _get_tree(tx, login):
+        Tree._get_account(tx,login)
         query=(
             'MATCH(a:account{login:$login}) - [k:CHILD *]->(n:Node) return id(n) as idn, n, id(a) as ida, a, k'
         )
@@ -26,7 +29,16 @@ class Tree():
         return [{"id":row["k"][0].id,type:row["k"][0].type, "nodes":row["k"][0].nodes } for row in result]
         #return [{"node":{"id": row["idn"] ,"name":row["n"]["name"]}, "account":{ "id": row["ida"] ,"name":row["a"]["login"]},"relation":{"r":row["k"]}} for row in result]
 
+    @staticmethod
+    def _get_account(tx, login):
+        query=(
+            "match (a:account {login:'pwujczyk1'}) return a"
+        )
+        tempresult=tx.run(query,login=login);
+        result=tempresult.single()[0]
+        account=Account(result.id,result._properties['login'])
 
+        print(id)
 
 
     def close(self):
