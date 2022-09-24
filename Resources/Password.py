63fdbd72b1b4ad54e41c2e6ed1ed2ad2b6642a11
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-09-24 13:47:18 +0200

    name of the password

--- Resources/Password.py
+++ Resources/Password.py
@@ -7,6 +7,6 @@ from flask import request, Response
 class PasswordResource(Resource):
     def get(self):
         client=secretmanager.SecretManagerServiceClient()
-        name=""
+        name="projects/488456392633/secrets/neo4jpassword/versions/1"
         r=client.access_secret_version(request={"name":name})
         return Response(str(r), mimetype="text/plain", direct_passthrough=True);
